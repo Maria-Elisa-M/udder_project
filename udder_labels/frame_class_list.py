fcb57cba94725c2a56534bd4eca1ffad2fb3decf
Maria <montes7@purdue.edu>
2023-12-12 09:43:54 -0600

    to label code done

--- udder_labels/frame_class_list.py
+++ udder_labels/frame_class_list.py
@@ -5,12 +5,12 @@ import pandas as pd
 # list all labels
 label_path = os.path.join(os.getcwd(), r'labels\class') 
 label_list = os.listdir(label_path)
-filenames = [file.replace(".txt", ".png") for file in label_list]
-cow_list = [file.split("_")[1] for file in label_list]
+filenames = [file.replace(".txt", "") for file in label_list]
+cow_list = [file.split("_")[0] for file in label_list]
 class_list = []
 for label in label_list:
     with open(os.path.join(label_path, label), "r") as f:
         class_list.append(f.read())
         
-df = pd.DataFrame({"filename":filenames , "cow":cow_list, "class": class_list})
+df = pd.DataFrame({"filename":filenames , "cow":cow_list, "frame_class": class_list})
 df.to_csv("frame_class_list.csv", index = False)
