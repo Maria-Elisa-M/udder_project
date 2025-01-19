b6ee721feb994b55102fa70d6685d4b33bfab6d1
Maria <montes7@purdue.edu>
2025-01-19 00:32:09 -0600

    started r analyis

--- udder_analysis/visit_yield_teat.R
+++ udder_analysis/visit_yield_teat.R
@@ -10,10 +10,26 @@ file_name <- "visit_ft_long.csv"
 file_path <- file.path(data_dir, file_name, fsep = .Platform$file.sep)
 df <- read.csv(file_path)
 
-# linear models
-df$teat <- as.factor(df$teat)
-df$cow <- as.factor(df$cow)
+# center and standardize values
+cols1 <- c("yield", "interval_sec", "vol", "days_in_milk",  "sarea", "area", "circ", "exc", "peri")
+df[cols1] <- scale(df[cols1])
 
-model <- lmer(yield ~ interval_sec + vol + days_in_milk + lactation_number + sarea + area + vol + teat + teat*vol + (1|cow),  data = df)
+# set as factors
+cols2 <- c("cow", "teat")
+df <- df%>%mutate_at(cols2, factor)
+
+# drop rows with missing values
+vars = c(cols1, cols2, "lactation_number")
+dfm <- df%>%select(all_of(vars))%>%drop_na()
+
+model <- lmer(yield ~ interval_sec + vol + days_in_milk + lactation_number + sarea + area + circ + peri + exc  + teat + (1|cow),  data = dfm)
 
 summary(model)
+
+#elimination of non-significant effects
+s <- step(model)
+
+#plot of post-hoc analysis of the final model
+plot(s)
+
+summary(s)
--- udder_analysis/visit_yield_udder.R
+++ udder_analysis/visit_yield_udder.R
@@ -1,5 +1,6 @@
 library(dplyr)
-library(lmerTest)
+library(tidyr)
+library(stats)
 
 # directories 
 wd <- "C:/Users/marie/rep_codes/udder_project/udder_analysis"
@@ -11,18 +12,34 @@ file_path <- file.path(data_dir, file_name, fsep = .Platform$file.sep)
 df <- read.csv(file_path)
 
 
-# linear models
+# center and standardize values
+cols <- c("yield","sarea_udder", "vol_udder", 
+          "days_in_milk", "area_udder",  "peri_udder",
+          "exc_udder", "circ_udder","interval_sec")
 
-model <- lm(yield ~ interval_sec + vol_udder + days_in_milk + lactation_number + sarea_udder + area_udder +
-              vol_udder*interval_sec + vol_udder*days_in_milk + lactation_number*days_in_milk, data = df)
+df <- df%>%mutate_at(cols, scale)
 
+# drop rows with missing values
+vars = c(cols, "lactation_number")
+dfm <- df[vars]%>%drop_na()
+
+#define intercept-only model
+intercept_only <- lm(yield ~ 1, data=dfm)
+summary(intercept_only)
+
+#define model with all predictors
+all <- lm(yield ~ ., data=dfm)
+summary(all)
+
+#stepwise regression
+forward <- step(intercept_only, direction='forward', scope=formula(all), trace=0)
+backward <- step(all, direction='backward', scope=formula(all), trace=0)
+both <-step(all, direction='both', scope=formula(all), trace=0)
+
+
+
+
+model <- lm(yield ~ interval_sec + lactation_number + vol_udder + 
+              peri_udder, data = df)
 summary(model)
-
-model <- lm(yield ~ interval_sec + vol_udder + days_in_milk + lactation_number + sarea_udder, data = df)
-
-summary(model)
-
-model <- lm(vol_udder ~ interval_sec + days_in_milk + lactation_number + yield + 
-               lactation_number*days_in_milk, data = df)
-
-summary(model)
+summary(forward)
