ff2f0976c62559996395a4d9605507d13464d218
Maria <montes7@purdue.edu>
2025-02-09 20:53:50 -0600

    update

--- udder_analysis/visit_yield_udder.R
+++ udder_analysis/visit_yield_udder.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(stats)
+library(ggplot2)
 
 # directories 
 wd <- "C:/Users/marie/rep_codes/udder_project/udder_analysis"
@@ -11,13 +12,21 @@ file_name <- "visit_ft_long_udder.csv"
 file_path <- file.path(data_dir, file_name, fsep = .Platform$file.sep)
 df <- read.csv(file_path)
 
+# check if the data meets the assumptions
+# yield
+hist(df$yield)
+qqnorm(df$yield)
+qqline(df$yield)
+# volume
+hist(df$vol_udder)
+qqnorm(df$vol_udder)
+qqline(df$vol_udder)
 
 # center and standardize values
 cols <- c("yield","sarea_udder", "vol_udder", 
           "days_in_milk", "area_udder",  "peri_udder",
           "exc_udder", "circ_udder","interval_sec")
-
-df <- df%>%mutate_at(cols, scale)
+df[cols] <- scale(df[cols])
 
 # drop rows with missing values
 vars = c(cols, "lactation_number")
@@ -31,15 +40,22 @@ summary(intercept_only)
 all <- lm(yield ~ ., data=dfm)
 summary(all)
 
-#stepwise regression
+# stepwise regression
 forward <- step(intercept_only, direction='forward', scope=formula(all), trace=0)
 backward <- step(all, direction='backward', scope=formula(all), trace=0)
 both <-step(all, direction='both', scope=formula(all), trace=0)
 
+summary(forward)
 
-
-
+# model using all the data
 model <- lm(yield ~ interval_sec + lactation_number + vol_udder + 
               peri_udder, data = df)
 summary(model)
-summary(forward)
+# residuals
+
+
+
+
+
+
+
