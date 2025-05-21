e14708300f7a729f43bad17448925dcfc00190df
Maria <montes7@purdue.edu>
2025-05-21 10:01:12 -0500

    nthng

--- udder_analysis/visit_yield_teat.R
+++ udder_analysis/visit_yield_teat.R
@@ -11,10 +11,15 @@ data_dir <- file.path(wd, "long_format_df", fsep = .Platform$file.sep)
 file_name <- "visit_ft_long.csv"
 file_path <- file.path(data_dir, file_name, fsep = .Platform$file.sep)
 df <- read.csv(file_path)
-df <- df%>%mutate(parity = ifelse(lactation_number > 1, 1, 0))
+dfo <- df
+df <- df%>%mutate(parity = ifelse(lactation_number > 1, 1, 0), 
+                  log_int = log(interval_sec))
 
 
-ggplot(df) + geom_point(aes(y = yield, x = area)) +facet_grid(vars(teat))
+ggplot(dfo) + geom_point(aes(y = yield, x = area)) +facet_wrap(~teat, scales = "free")
+ggplot(dfo) + geom_point(aes(y = interval_sec, x = vol)) +facet_wrap(~teat, scales = "free")
+
+vms_df <- dfo%>%group_by(device_name, lactation_number)%>%summarise(count =n())
 
 # center and standardize values
 cols1 <- c("yield", "interval_sec", "vol", "days_in_milk",  "sarea", "area", "circ", "exc", "peri")
@@ -25,7 +30,7 @@ cols2 <- c("cow", "teat", "parity")
 df <- df%>%mutate_at(cols2, factor)
 
 # drop rows with missing values
-vars = c(cols1, cols2, "lactation_number", "parity")
+vars = c(cols1, cols2, "lactation_number", "parity", "device_name", "log_int")
 dfm <- df%>%select(all_of(vars))%>%drop_na()
 
 # model with individaul effects
@@ -34,7 +39,7 @@ summary(model)
 
 MuMIn::r.squaredGLMM(model)
 
-model2 <- lmer(yield ~ interval_sec + vol + days_in_milk + parity + circ + exc  + teat + (1|cow),  data = dfm)
+model2 <- lm(yield ~ interval_sec + vol + days_in_milk + lactation_number + sarea + area + circ + peri + exc  + teat ,  data = dfm)
 summary(model2)
 
 MuMIn::r.squaredGLMM(model2)
@@ -52,7 +57,12 @@ qqline(residuals(final))
 plot(residuals(final))
 
 
-
+# get predictions
+new <- tibble(area = dfm$area, teat = dfm$teat, interval_sec = mean(dfm$interval_sec), lactation_number = 1, cow = dfm$cow[1])
+new$pred <- predict(final, new)
+dfm$pred <- predict(final, new)
+ggplot(dfm) + geom_line(aes(y = pred, x = area)) + geom_point(aes(y = yield, x = area), color = "red") +facet_wrap(~teat, scales = "free")
+ggplot(dfo) + geom_point(aes(y = yield, x = log(interval_sec)))
 
 # model with intefinal# model with interactions
 model <- lmer(yield ~ lactation_number + interval_sec + area * teat + (1|cow), data = dfm)
