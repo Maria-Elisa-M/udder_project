fa238d55505bb00e9aa021a02e7beda43f7921c8
maria <montesgonzal@wisc.edu>
2026-01-24 08:30:48 -0600

    revision B

--- paper/revision_B/regression_all_revised.R
+++ paper/revision_B/regression_all_revised.R
@@ -4,7 +4,8 @@ library(lmerTest)
 library(ggplot2)
 
 # directories 
-wd <- "C:/Users/marie/rep_codes/udder_project/paper/revision"
+wd <- "C:/Users/marie/rep_codes/udder_project/paper/revision_B"
+setwd(wd)
 data_dir <- file.path(wd, "data", fsep = .Platform$file.sep)
 
 # read data
@@ -12,14 +13,15 @@ file_name <- "visit_ft_long.csv"
 file_path <- file.path(data_dir, file_name, fsep = .Platform$file.sep)
 df <- read.csv(file_path)
 df <- df%>%mutate(parity = ifelse(lactation_number > 1, 1, 0), 
-                  location = ifelse((teat == "lf")|(teat == "rf"), "front", "rear"))
+                  location = ifelse((teat == "lf")|(teat == "rf"), "front", "rear"), 
+                  side = ifelse((teat == "lf")|(teat == "lr"), "left", "right"))
 
 # center and standardize values
 cols1 <- c("yield", "interval_sec", "vol", "days_in_milk",  "sarea", "area", "circ", "exc", "peri", "conductivity", "peak_flow", "mean_flow", "len")
 df[cols1] <- scale(df[cols1])
 
 # set as factors
-cols2 <- c("cow", "teat", "parity", "location")
+cols2 <- c("cow", "teat", "parity", "location", "side")
 df <- df%>%mutate_at(cols2, factor)
 
 # drop rows with missing values
@@ -27,28 +29,56 @@ vars = c(cols1, cols2)
 dfm <- df%>%select(all_of(vars))%>%drop_na()
 
 # visit quarter yield
-model_yield <- lmer(yield ~ interval_sec + len + area + days_in_milk + parity + circ + exc + location + (1|cow),  data = dfm)
+model_yield <- lmer(yield ~ interval_sec + len + area + days_in_milk + parity + circ + exc + location + side + (1|cow),  data = dfm)
 summary(model_yield)
 resid <- residuals(model_yield)
+
+png(filename = "model/qqplot_yield.png", width = 800, height = 600)
 qqnorm(resid)
+dev.off()
+
+png(filename = "model/rfplot_yield.png", width = 800, height = 600)
+plot(model_yield, which = 1)
+dev.off()
 
 # visit quarter electical conductivity
-model_ec <- lmer(conductivity ~ yield + interval_sec + len + area + days_in_milk + parity + circ + exc + location + (1|cow),  data = dfm)
+model_ec <- lmer(conductivity ~ yield + interval_sec + len + area + days_in_milk + parity + circ + exc + location + side +(1|cow),  data = dfm)
 summary(model_ec)
 resid <- residuals(model_ec)
+
+png(filename = "model/qqplot_ec.png", width = 800, height = 600)
 qqnorm(resid)
+dev.off()
+
+png(filename = "model/rfplot_ec.png", width = 800, height = 600)
+plot(model_ec, which = 1)
+dev.off()
 
 # model quarter peak flow
-model_pf <- lmer(peak_flow ~ yield + interval_sec + len + area + days_in_milk + parity + circ + exc + location + (1|cow),  data = dfm)
+model_pf <- lmer(peak_flow ~ yield + interval_sec + len + area + days_in_milk + parity + circ + exc + location + side + (1|cow),  data = dfm)
 summary(model_pf)
 resid <- residuals(model_pf)
+
+png(filename = "model/qqplot_pf.png", width = 800, height = 600)
 qqnorm(resid)
+dev.off()
+
+png(filename = "model/rfplot_pf.png", width = 800, height = 600)
+plot(model_pf, which = 1)
+dev.off()
 
 # model quarter mean flow
-model_mf <- lmer(mean_flow ~ yield + interval_sec +len+ area + days_in_milk + parity + circ + exc + location + (1|cow),  data = dfm)
+model_mf <- lmer(mean_flow ~ yield + interval_sec +len+ area + days_in_milk + parity + circ + exc + location + side + (1|cow),  data = dfm)
 summary(model_mf)
 resid <- residuals(model_mf)
+
+png(filename = "model/qqplot_mf.png", width = 800, height = 600)
 qqnorm(resid)
+dev.off()
+
+png(filename = "model/rfplot_mf.png", width = 800, height = 600)
+plot(model_mf, which = 1)
+dev.off()
 
 # save the coeffients in a table
 coef_yield <- data.frame(coef(summary(as(model_yield,"lmerModLmerTest")))[, 1])
@@ -82,18 +112,18 @@ colnames(serror_mf) <- "mean_flow"
 merged_coef <- merge(coef_yield, coef_ec, by = 0, all=TRUE)
 merged_coef <- merge(merged_coef, coef_mf, by.x = 'Row.names', by.y = 0, all=TRUE)
 merged_coef <- merge(merged_coef, coef_pf, by.x = 'Row.names', by.y = 0, all=TRUE)
-merged_coef[, 2:5] <- round(merged_coef[, 2:5], 3)
+merged_coef[, 2:5] <- round(merged_coef[, 2:5], 2)
 
 merged_pv <- merge(pv_yield, pv_ec, by = 0, all=TRUE)
 merged_pv <- merge(merged_pv, pv_mf, by.x = 'Row.names', by.y = 0, all=TRUE)
 merged_pv <- merge(merged_pv, pv_pf, by.x = 'Row.names', by.y = 0, all=TRUE)
-merged_pv[, 2:5] <- round(merged_pv[, 2:5], 3)
+merged_pv[, 2:5] <- round(merged_pv[, 2:5], 2)
 
 merged_error <- merge(serror_yield, serror_ec, by = 0, all=TRUE)
 merged_error <- merge(merged_error, serror_mf, by.x = 'Row.names', by.y = 0, all=TRUE)
 merged_error <- merge(merged_error, serror_mf, by.x = 'Row.names', by.y = 0, all=TRUE)
-merged_error[, 2:5] <- round(merged_error[, 2:5], 3)
+merged_error[, 2:5] <- round(merged_error[, 2:5], 2)
 
-write.csv(merged_coef, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision\\tables\\merged_coef.csv", row.names = FALSE)
-write.csv(merged_pv, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision\\tables\\merged_pv.csv", row.names = FALSE)
-write.csv(merged_error, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision\\tables\\merged_error.csv", row.names = FALSE)
+write.csv(merged_coef, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision_B\\tables\\merged_coef.csv", row.names = FALSE)
+write.csv(merged_pv, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision_B\\tables\\merged_pv.csv", row.names = FALSE)
+write.csv(merged_error, "C:\\Users\\marie\\rep_codes\\udder_project\\paper\\revision_B\\tables\\merged_error.csv", row.names = FALSE)
